0ca42dd71d3b1627c4db5111d4d4fef10a3327b7
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-25 08:11:57 -0700

    more_correction

--- 1-opcode_functions.c
+++ 1-opcode_functions.c
@@ -69,35 +69,3 @@ void sub_function(stack_t **stack, unsigned int line_number)
 	*stack = temp->next;
 	free(temp);
 }
-
-/**
- * div_function - Divides top two elements
- * @stack: Pointer to stack
- * @line_number: Line number
- */
-void div_function(stack_t ** stack, unsigned int line_number)
-{
-	stack_t *temp;
-
-	if (!stack || !*stack || !(*stack)->next)
-	{
-		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line_number);
-		fclose(fd);
-		free_stack(*stack);
-		free(buffer);
-		exit(EXIT_FAILURE);
-	}
-	temp = *stack;
-	if (temp->n == 0)
-	{
-		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
-		fclose(fd);
-		free_stack(*stack);
-		free(buffer);
-		exit(EXIT_FAILURE);
-	}
-	temp->next->n /= temp->n;
-	temp->next->prev = NULL;
-	*stack = temp->next;
-	free(temp);
-}
