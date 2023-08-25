82a2194b17d46314d35a2f996a297634acfe3e01
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-25 06:57:42 -0700

    Task 6: sub opcode implementaion

--- 1-opcode_functions.c
+++ 1-opcode_functions.c
@@ -45,3 +45,27 @@ void add_function(stack_t **stack, unsigned int line_number)
 	*stack = temp->next;
 	free(temp);
 }
+
+/**
+ * sub_function - Subtact top element from the second one
+ * @stack: Pointer to stack
+ * @line_number: Line number
+ */
+void sub_function(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!stack || !*stack || (*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number);
+		fclose(fd);
+		free_stack(*stack);
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	temp->next->n -= temp->n;
+	temp->next->prev = NULL;
+	*stack = temp->next;
+	free(temp);
+}
