000b097aa59cd8741780cb77ea87139b8756d364
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-26 08:18:30 -0700

    nop opcode implementation

--- monty_functions.c
+++ monty_functions.c
@@ -67,6 +67,7 @@ int executor(int line_number, char *command, char *arg, stack_t **stack)
 		{"swap", swap_function},
 		{"add", add_function},
 		{"sub", sub_function},
+		{"nop", nop_function},
 		{NULL, NULL}
 	};
 
